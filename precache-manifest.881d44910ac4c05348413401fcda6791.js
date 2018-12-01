7510582fb82658546ee0ef94a02355439cf95393
David Lee <concoctionfactory@gmail.com>
2018-12-01 15:32:05 -0500

    Updates

---
+++ precache-manifest.881d44910ac4c05348413401fcda6791.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "baa0024e2b07ba0eeb2e",
+    "url": "/react-design-portfolio/static/js/runtime~main.baa0024e.js"
+  },
+  {
+    "revision": "7c52b72a4208b8100bf6",
+    "url": "/react-design-portfolio/static/js/main.7c52b72a.chunk.js"
+  },
+  {
+    "revision": "3be7184e22cdbfc6e06f",
+    "url": "/react-design-portfolio/static/js/1.3be7184e.chunk.js"
+  },
+  {
+    "revision": "7c52b72a4208b8100bf6",
+    "url": "/react-design-portfolio/static/css/main.c8f17092.chunk.css"
+  },
+  {
+    "revision": "3be7184e22cdbfc6e06f",
+    "url": "/react-design-portfolio/static/css/1.7644235f.chunk.css"
+  },
+  {
+    "revision": "fedfe391ac60889c46da6d35b199e3a9",
+    "url": "/react-design-portfolio/index.html"
+  }
+];
