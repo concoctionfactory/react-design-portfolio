315425090b4026732814e278e6d197997d5de502
David Lee <concoctionfactory@gmail.com>
2018-12-01 16:11:45 -0500

    Updates

--- precache-manifest.2e91808eb3163b7d6ab54b3612c0a372.js
+++ precache-manifest.2e91808eb3163b7d6ab54b3612c0a372.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/react-design-portfolio/static/js/runtime~main.baa0024e.js"
   },
   {
-    "revision": "7c52b72a4208b8100bf6",
-    "url": "/react-design-portfolio/static/js/main.7c52b72a.chunk.js"
+    "revision": "638069d499018772b832",
+    "url": "/react-design-portfolio/static/js/main.638069d4.chunk.js"
   },
   {
     "revision": "3be7184e22cdbfc6e06f",
     "url": "/react-design-portfolio/static/js/1.3be7184e.chunk.js"
   },
   {
-    "revision": "7c52b72a4208b8100bf6",
+    "revision": "638069d499018772b832",
     "url": "/react-design-portfolio/static/css/main.c8f17092.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/react-design-portfolio/static/css/1.7644235f.chunk.css"
   },
   {
-    "revision": "fedfe391ac60889c46da6d35b199e3a9",
+    "revision": "288b8b9e323f71fee103d26e211517a4",
     "url": "/react-design-portfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-design-portfolio/precache-manifest.881d44910ac4c05348413401fcda6791.js"
+  "/react-design-portfolio/precache-manifest.2e91808eb3163b7d6ab54b3612c0a372.js"
 );
 
 workbox.clientsClaim();
